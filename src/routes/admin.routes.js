a2d3342fce3e0336ec8d712eb2a3b04a71ffc4ac
Mohseen-dev <m9084097960@gmai.com>
2026-05-01 13:44:42 +0530

    name correction

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -2,8 +2,8 @@ import Router from "express";
 import {signUp} from "../controllers/admin/signUp.controller.js";
 import {loginAdmin} from "../controllers/admin/login.controller.js";
 import {authVerify} from '../Middlewares/admin/authVerify.js'
-import {createBus} from '../controllers/admin/createBus.controller.js'
-import { createDriver } from "../controllers/admin/createDriver.controller.js";
+import {createBus} from '../controllers/admin/Bus.controller.js'
+import { createDriver } from "../controllers/admin/Driver.controller.js";
 const router = Router();
 
 // router.post("/signup",signUp);  just for testing purpose, later we will remove this route and create admin directly in database
