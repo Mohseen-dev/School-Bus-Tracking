76ed3758a945b26cf2d2c39b89ec27e8555b5cfe
Mohseen-dev <m9084097960@gmai.com>
2026-07-05 00:00:39 +0530

    v1 backend complete

--- src/routes/parent.routes.js
+++ src/routes/parent.routes.js
@@ -4,15 +4,19 @@ import { getProfile } from "../controllers/parent/getProfile.controller.js";
 import { currentTripForStudent } from "../controllers/parent/currentTripForStudent.controller.js";
 import { studentStatus } from "../controllers/parent/studentStatus.controller.js";
 import { busLocation } from "../controllers/parent/busLocation.controller.js";
-import {parentAuthVerify} from '../Middlewares/parent/parentAuthVerify.js'
+import { parentAuthVerify } from "../Middlewares/parent/parentAuthVerify.js";
 import { eta } from "../controllers/parent/eta.controller.js";
 const parentRouter = express.Router();
 
-parentRouter.post("/login", parentLogin);//done
-parentRouter.get("/get-profile",parentAuthVerify, getProfile);//done
-parentRouter.get("/student-status/:studentId", parentAuthVerify,studentStatus);//done
-parentRouter.get("/current-trip/:studentId",parentAuthVerify, currentTripForStudent);//done
-parentRouter.get("/bus-location/:tripId",parentAuthVerify, busLocation);
-parentRouter.get("/eta/:studentId",parentAuthVerify, eta);
+parentRouter.post("/login", parentLogin); //done
+parentRouter.get("/get-profile", parentAuthVerify, getProfile); //done
+parentRouter.get("/student-status/:studentId", parentAuthVerify, studentStatus); //done
+parentRouter.get(
+  "/current-trip/:studentId",
+  parentAuthVerify,
+  currentTripForStudent
+); //done
+parentRouter.get("/bus-location/:tripId", parentAuthVerify, busLocation);
+parentRouter.get("/eta/:studentId", parentAuthVerify, eta);
 
 export default parentRouter;
